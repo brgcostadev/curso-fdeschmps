92a1a33a67a458dc960ab46ebf0c3d463a9ab456
Bruno Gonçalves Costa <br.gcosta@hotmail.com>
2023-08-22 18:53:08 +0000

    implementações

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
 function Home() {
-    return <h1 className="teste">Aqui vai ficar salvo o projeto do JEI!!!!</h1>
+    return (
+        <>
+            <h1 className="teste">Aqui vai ficar salvo o projeto do JEI!!!!</h1>
+            <h2>Fazer header</h2>
+        </>
+    )
 }
 
 export default Home;
